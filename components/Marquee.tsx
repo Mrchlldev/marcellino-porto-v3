46b17c0c60217bec6a63a551daff6cc534b92f69
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2026-06-18 20:10:14 +0700

    Upload components/Marquee.tsx

---
+++ components/Marquee.tsx
@@ -0,0 +1,7 @@
+export function Marquee() {
+  return (
+    <div className="marquee border-y-4 border-black bg-[#171717] py-4 font-title text-3xl font-black text-[#fff7df]">
+      <span>RETRO UI • MODERN PORTFOLIO • MARCELLINO F.A • NEXT.JS • TYPESCRIPT • TAILWIND CSS • RETRO UI • MODERN PORTFOLIO • MARCELLINO F.A • NEXT.JS • TYPESCRIPT • TAILWIND CSS • </span>
+    </div>
+  );
+}
